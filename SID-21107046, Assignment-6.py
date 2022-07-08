d9c2bc537b9624349ee46bb25c400e8fa6ceed16
yuvraj12ger <98840424+yuvraj12ger@users.noreply.github.com>
2022-07-08 19:15:58 +0530

    Add files via upload

---
+++ SID-21107046, Assignment-6.py
@@ -0,0 +1,117 @@
+# Question1
+
+def perfect_number(n):
+    sum = 0
+    for x in range(1, n):
+        if n % x == 0:
+            sum += x
+    return sum == n
+print(perfect_number(6))
+
+# Question2
+def isPalindrome(string):
+	left_pos = 0
+	right_pos = len(string) - 1
+	
+	while right_pos >= left_pos:
+		if not string[left_pos] == string[right_pos]:
+			return False
+		left_pos += 1
+		right_pos -= 1
+	return True
+print(isPalindrome('aza'))
+
+# Question3
+def pascal_triangle(n):
+   trow = [1]
+   y = [0]
+   for x in range(max(n,0)):
+      print(trow)
+      trow=[l+r for l,r in zip(trow+y, y+trow)]
+   return n>=1
+pascal_triangle(6)
+
+# Question4
+import string, sys
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    alphaset = set(alphabet)
+    return alphaset <= set(str1.lower())
+ 
+print ( ispangram('The quick brown fox jumps over the lazy dog'))
+
+# Question5
+items=[n for n in input().split('-')]
+items.sort()
+print('-'.join(items))
+
+# Question6
+def student_data(student_id, **kwargs):
+    print(f'\nStudent ID: {student_id}')
+    if 'student_name' in kwargs:
+        print(f"Student Name: $ {kwargs['student_name']}")
+    
+    if 'student_name' and 'student_class' in kwargs:
+            print(f"\nStudent Name: $ {kwargs['student_name']}")
+            print(f"Student Class: $ {kwargs['student_class']}")
+
+ 
+student_data(student_id='SV12', student_name='Jean Garner')
+
+student_data(student_id='SV12', student_name='Jean Garner', student_class ='V')
+
+# Question7
+class Student:
+    pass 
+class Marks:
+    pass 
+student1 = Student()
+marks1 = Marks()
+print(isinstance(student1, Student))
+print(isinstance(marks1, Student))
+print(isinstance(marks1, Marks)) 
+print(isinstance(student1, Marks))
+print("\nCheck whether the said classes are subclasses of the built-in object class or not.")
+print(issubclass(Student, object))
+print(issubclass(Marks, object))
+
+# Question8
+class py_solution:
+ def threeSum(self, nums):
+        nums, result, i = sorted(nums), [], 0
+        while i < len(nums) - 2:
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+            i += 1
+            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
+                i += 1
+        return result
+
+print (py_solution().threeSum([-25, -10, -7, -3, 2, 4, 8, 10]))
+
+# Question9
+class py_solution:
+   def is_valid_parenthese(self, str1):
+        stack, pchar = [], {"(": ")", "{": "}", "[": "]"}
+        for parenthese in str1:
+            if parenthese in pchar:
+                stack.append(parenthese)
+            elif len(stack) == 0 or pchar[stack.pop()] != parenthese:
+                return False
+        return len(stack) == 0
+
+print(py_solution().is_valid_parenthese("(){}[]"))
+print(py_solution().is_valid_parenthese("()[{)}"))
+print(py_solution().is_valid_parenthese("()"))
+
+
